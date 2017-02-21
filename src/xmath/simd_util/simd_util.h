6f285cb49155eeb90b5685b15e9d2df0f001a177
lyrahgames <markuspawellek@gmail.com>
2017-02-22 00:42:34 +0100

    Implement AOS-to-SOA-functions for bare SIMD-types

--- src/xmath/simd_util/simd_util.h
+++ src/xmath/simd_util/simd_util.h
@@ -17,11 +17,30 @@ inline __m256d dot3(const __m256d* a, const __m256d* b){
 
 
 inline __m256d sqlen3(const __m256d& v_x, const __m256d& v_y, const __m256d& v_z){
-	return dot3(a_x, a_y, a_z, a_x, a_y, a_z);
+	return dot3(v_x, v_y, v_z, v_x, v_y, v_z);
 }
 
 inline __m256d sqlen3(const __m256d* v){
 	return dot3(v, v);
+}
+
+inline void aos_to_soa_3(__m128& v1, __m128& v2, __m128& v3){
+	const __m128 tmp1 = _mm_shuffle_ps(v1, v2, 0x49);
+	const __m128 tmp2 = _mm_shuffle_ps(v2, v3, 0x9e);
+
+	v1 = _mm_shuffle_ps(v1, tmp2, 0x8c);
+	v2 = _mm_shuffle_ps(tmp1, tmp2, 0xd8);
+	v3 = _mm_shuffle_ps(tmp1, v3, 0xcd);
+}
+
+inline void aos_to_soa_3(__m256d& v1, __m256d& v2, __m256d& v3){
+	const __m256d tmp1 = _mm256_permute2f128_pd(v1, v2, 0x30);
+	const __m256d tmp2 = _mm256_permute2f128_pd(v1, v3, 0x21);
+	const __m256d tmp3 = _mm256_permute2f128_pd(v2, v3, 0x30);
+
+	v1 = _mm256_shuffle_pd(tmp1, tmp2, 0x0a);
+	v2 = _mm256_shuffle_pd(tmp1, tmp3, 0x05);
+	v3 = _mm256_shuffle_pd(tmp2, tmp3, 0x0a);
 }
 
 
