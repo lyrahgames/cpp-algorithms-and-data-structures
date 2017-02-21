b2688c9d46e362551e9d8a0730d876beae7ba86c
lyrahgames <markuspawellek@gmail.com>
2017-02-21 14:16:46 +0100

    Add file 'xmath.h' for standard include files

---
+++ src/xmath/xmath.h
@@ -0,0 +1,19 @@
+#pragma once
+// #ifndef __XMATH_H__
+// #define __XMATH_H__
+
+#include <xmath/vec.h>
+#include <xmath/simd_util/v256d.h>
+#include <xmath/simd_util/vec_v256d.h>
+
+
+namespace xmath{
+
+
+
+
+
+} // xmath
+
+
+// #endif // __XMATH_H__
