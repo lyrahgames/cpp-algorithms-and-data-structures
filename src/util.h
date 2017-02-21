5182c14567ae7766e7fb1bdabdb8fb69e112b50b
lyrahgames <markuspawellek@gmail.com>
2017-02-22 00:59:59 +0100

    Add file 'util.h' for standard utilities
    This file can be used as a standard or template file for other projects.

---
+++ src/util.h
@@ -0,0 +1,26 @@
+#pragma once
+// #ifndef __UTIL_H__
+// #define __UTIL_H__
+
+#include <xmath/xmath.h>
+
+
+using namespace xmath;
+using namespace xmath::simd_util;
+
+
+using uint = unsigned int;
+using byte = unsigned char;
+
+using vecf2 = vec<float,2>;
+using vecf3 = vec<float,3>;
+using vecf4 = vec<float,4>;
+
+using vecd2 = vec<double,2>;
+using vecd3 = vec<double,3>;
+using vecd4 = vec<double,4>;
+
+
+
+
+// #endif // __UTIL_H__
